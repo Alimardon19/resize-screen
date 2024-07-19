8f9986b5e304b58e22804b593d5dd591cc50519a
Alimardon19 <alimardondjumanaliyev07@gmail.com>
2024-07-19 16:53:26 +0500

    update

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-var s1920 = 1920;
-var s1366 = 1366;
-function sizeCalculate(ekranEni, miqdor) {
-    var size = (ekranEni * miqdor) / 1920;
-    return size;
-}
+addEventListener("DOMContentLoaded", () => {
+    const from = localStorage.getItem("from") || window.innerWidth;
+    const to = localStorage.getItem("to") || 0;
+    
+    // set value
+    document.getElementById("from").value = from;
+    document.getElementById("to").value = to;
 
-
-document.getElementById("input").addEventListener("keyup", (e) => {
-    const value = e.target.value;
-    let size = Math.round(sizeCalculate(s1366, value));
-    document.getElementById("result").innerText = size;
-    document.title = `Resize screen | ${value} = ${size}`
+    function sizeCalculate(ekranEni, miqdor) {
+        var size = (ekranEni * miqdor) / from;
+        return size;
+    }
+    
+    document.getElementById("input").addEventListener("keyup", (e) => {
+        const value = e.target.value;
+        let size = Math.round(sizeCalculate(to, value));
+        document.getElementById("result").innerText = size;
+        document.title = `${value} = ${size}`
+    });
 });
+
+// save to localStorage
+const saveToLocal = (event) => {
+    localStorage.setItem(event.target.name, event.target.value);
+};
+
+// get the incoming values`
+document.getElementById("from").addEventListener("keyup", saveToLocal)
+document.getElementById("to").addEventListener("keyup", saveToLocal)
